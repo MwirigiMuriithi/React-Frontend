51924ec0a45565f38b378873d6959b2b349709f1
MwirigiMuriithi <learnerviner@gmail.com>
2024-07-12 13:00:18 +0300

    changed login and signup to use email

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,27 @@
 import { useState } from 'react';
 import { useLogin } from '../../hooks/useLogin';
-import './Login.css'
+import './Login.css';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const { login, error, isLoading } = useLogin();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        await login(username, password);
+        await login(email, password);
     };
 
     return (
         <div className="login-container">
             <h2>Pick up where you left.</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
+                <label htmlFor="email">Email:</label>
                 <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                 />
                 <label htmlFor="password">Password:</label>
